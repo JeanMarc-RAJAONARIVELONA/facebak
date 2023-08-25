9d47a820be3b497d5959e18a0b58a5569b954522
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona@gmail.com>
2023-08-25 09:02:49 +0300

    fix : comment home page to see how prifil page look

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-import Home from "./page/Home";
+import Profil from "./page/Profil";
+// import Home from "./page/Home";
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Profil/>
     </>
   );
 }
