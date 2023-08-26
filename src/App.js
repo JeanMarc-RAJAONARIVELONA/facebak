6c4eed327261cafaa118d990acaa0fd0bedce171
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona@gmail.com>
2023-08-26 09:00:04 +0300

    feat : add router in project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Profil from "./page/Profil";
-// import Home from "./page/Home";
+import Home from "./page/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-      {/* <Home /> */}
-      <Profil/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path= "/" element={<Home/>}></Route>
+       <Route exact path="/Home" element={<Home />}></Route>
+       <Route exact path="/Profil" element={<Profil/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
