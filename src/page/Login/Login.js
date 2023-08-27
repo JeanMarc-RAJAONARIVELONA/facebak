53fc898a265c788a196b85825276e222c2867a09
Alpha <hei.ronaldino@gmail.com>
2023-08-27 17:47:23 +0300

    Feat: add some option in login and sign up

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import './css.css'
 import Register from "../Register/Register";
+import { Link } from 'react-router-dom';
 export default function Login() {
   return (
     <>
-    <div class="title">
+    <div className ="title">
         <h1>Login</h1>
     </div>
         <form action="">
-            <div class="item-container">
+            <div className="item-container">
        
-                <div class="little-title">
+                <div className="little-title">
                     <h2>Sign In Your Account</h2>
                 </div>
-                <div class="input-container">
+                <div className="input-container">
                     <input type="text"  name="Username" placeholder="Username"/>
                     <input type="password" name="password" id="password" placeholder="Password"/>
                  
                     <label >Remember me
                         <input type="checkbox" name="remember"/>
                     </label>
-                  <button class="btn">Login</button>
+                  <button className="btn">Login</button>
                 </div>
-                <div class="pass-forget">
-                    <p class="lost-password">Lost your password ?</p>
-                    <p class="register" onClick={Register}>Registration</p>
+                <div className="pass-forget">
+                    <p className="lost-password">Lost your password ?</p>
+                    <Link to="/register" className="register">Registration</Link>
                 </div>
                 
             </div>
--- src/page/Register/Register.jsx
+++ src/page/Register/Register.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 import './register.css'
+import { Link } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 export default function Register() {
+  
+  const iconStyle = {
+    color: '#34b7f1',
+};
   return (
     <>
-    <div class="title">
+    <div className="title">
             <h1>Facebak</h1>
         </div>
 
            <form action="">
-            <div class="item-container">
-                <div class="little-title">
+            <div className ="item-container">
+                <div className="little-title">
                     <h2>
                       
                         Create Your Account</h2>
                         
                 </div>
-                <div class="input-container">
+                <div className="input-container">
                     <input type="text"  name="Username" placeholder="Lastname"/>
                     <input type="password" name="password" id="password" placeholder="Firstname"/>
                     <input type="password" name="password" id="password" placeholder="Password"/>
                     <input type="password" name="password" id="password" placeholder="Confirm Password"/>
                  
                    
-                  <button class="btn">Create</button>
+                  <button className="btn">Create</button>
+    
+                  <div className='Back'>
+                      <Link to="/" className='back-login'><ArrowBackIcon style={iconStyle}/>Back to login</Link>
+                  </div>
                 </div>
             
                
