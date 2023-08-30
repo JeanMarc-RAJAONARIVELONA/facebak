1708711b9d927af7e82fb79f24b983e415dd0b97
Alpha <hei.ronaldino@gmail.com>
2023-08-30 03:52:46 +0300

    Create login acces and authentification

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,23 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './css.css'
 import Register from "../Register/Register";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [isMessageVisible, setMessageVisible] = useState(false);
+    const navigate = useNavigate();
+
+  useEffect(() => {
+    if (errorMessage) {
+      setMessageVisible(true);
+      setTimeout(() => {
+        setMessageVisible(false);
+        setErrorMessage("");
+      }, 3000); 
+    }
+  }, [errorMessage]);
+
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        const userData = {
+          username,
+          email,
+          password
+        };
+    
+        try {
+          const response = await axios.post("http://localhost:8080/users/login", userData);
+    
+          if (response.status === 200) {
+           Cookies.set("acces",response.data.token);
+           navigate("/home");
+          }
+        } catch (error) {
+           console.error("Erreur lors de la connection de compte :", error);
+           setErrorMessage("erreur lors de la connection de compte");
+        }
+    
+      };
   return (
     <>
     <div className ="title">
         <h1>Login</h1>
     </div>
-        <form action="">
+    <div className={`error-message ${isMessageVisible ? "show-message" : ""}`}>
+        {errorMessage}
+      </div>
+        <form onSubmit={handleFormSubmit}>
             <div className="item-container">
        
                 <div className="little-title">
                     <h2>Sign In Your Account</h2>
                 </div>
                 <div className="input-container">
-                    <input type="text"  name="Username" placeholder="Username"/>
-                    <input type="password" name="password" id="password" placeholder="Password"/>
-                 
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
+             <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+        <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+            
                     <label >Remember me
                         <input type="checkbox" name="remember"/>
                     </label>
